eadd44bddc880af3e8dd4d73a0d6651fffba09b0
jovany velez <jovany.velez@udea.edu.co>
2023-05-07 16:12:55 -0500

    endpoint1

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,6 +8,13 @@ export const handle: Handle = async ({ event, resolve }) => {
 	// Apply CORS header for API routes
 	if (event.url.pathname.startsWith('/api/import')) {
 		// Required for CORS to work
+		/**
+		 * Cuando se realiza una solicitud HTTP con un método o un encabezado personalizado
+		 * que no está permitido por la política de CORS, el navegador envía una solicitud
+		 * Preflight OPTIONS al servidor para obtener permiso antes de enviar la solicitud real. 
+		 * La solicitud Preflight OPTIONS incluye el método y los encabezados que se utilizarán en 
+		 * la solicitud real, así como la dirección URL y el origen del sitio que realiza la solicitud.
+		 */
 		if (event.request.method === 'OPTIONS') {
 			return new Response(JSON.stringify({success:'Soporta CORS'}), {
 				headers: {
